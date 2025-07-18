3ffeb72f324630d07e455ff47b7268f84c94948c
2025-123 <iancarlosvasquezl92@gmail.com>
2025-07-18 00:52:52 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,3 @@
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('radio-rodadero-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './logo.jpg'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').catch(err => console.error(err));
+}
